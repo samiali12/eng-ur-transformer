d4b72f78545c9865c8cf302b715c93e69836acb4
Sami Ali <sami_jameel@nextpack.org>
2025-09-18 09:45:21 +0500

    implement positional encoding and feedforward layer

--- model.py
+++ model.py
@@ -13,3 +13,60 @@ class InputEmbedding(nn.Module):
 
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        self.d_model =  d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(seq_len, d_model)
+        pos = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(pos * div_term)
+        pe[:, 0::1] = torch.cos(pos * div_term)
+
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, x.shape[1], :] 
+        return self.dropout(x)
+    
+
+class LayerNormalizaion(nn.Module):
+    def __init__(self, eps: float = 1e-06):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.beta = nn.Parameter(torch.zeros(1))
+    
+    def forward(self, x):
+        mean = torch.mean(x, dim=-1, keepdim=True)
+        std = torch.std(x, dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (self.eps + std) + self.beta
+
+class FeedForwardLayer(nn.Module):
+    def __init__(self, d_model:  int, d_ff: int, dropout: int):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = torch.relu(x)
+        x = self.dropout(x)
+        x = self.linear_2(x)
+        return x 
+
+
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, d_model: int, num_head: int, dropout: int):
+        self.d_model = d_model
+        self.num_head = num_head
+    
+        assert d_model % num_head == 0, "d_model must be divisible by num_heads"
